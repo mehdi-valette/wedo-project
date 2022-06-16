4f4a859bda32c1cce56ff3bd97af7b5022a2d01a
Mehdi Valette <mehdi.valette@gmail.com>
2022-06-16 23:24:00 +0200

    fix: the block needs to know an eventKey for the accordion's item

--- wedo-saving/src/components/Block.js
+++ wedo-saving/src/components/Block.js
@@ -3,7 +3,7 @@ import Accordion from 'react-bootstrap/Accordion';
 
 export default function Block(props) {
   return(
-  <Accordion.Item eventKey="0">
+  <Accordion.Item eventKey={props.eventKey}>
     <Accordion.Header>
       {props.title}
     </Accordion.Header>
